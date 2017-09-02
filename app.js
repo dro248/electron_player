fd80d64b999f15bef01994a517dca6c0673c6507
David Ostler <david.ostler001@gmail.com>
2017-09-02 14:14:33 -0600

    play button now launches player

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var electron = require('electron')  
-       
+
+// Keep a global reference of the window object so that it isn't garbage collected
+var mainWindow = null
+
+
+// This method will be called when Electron has finished
+// initializing and is ready to create browser windows.
 electron.app.on('ready', function () {  
-	var mainWindow = new electron.BrowserWindow({width: 1200, height: 800})  
+	// Create the Splash Screen window
+	mainWindow = new electron.BrowserWindow({width: 1200, height: 800, frame: false})  
 	mainWindow.loadURL('file://' + __dirname + '/splashScreen.html')  
+	// mainWindow.loadURL('file://' + __dirname + '/index.html')  
 })
