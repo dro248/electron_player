245a4417c57ff4fced005e2acd9c1b63c7e2384e
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2018-11-29 17:16:00 -0700

    add HH:MM:SS labels to hummedia json

--- scripts/hummedia2ic.py
+++ scripts/hummedia2ic.py
@@ -2,8 +2,10 @@ import json
 from pathlib import Path
 import sys
 
+from hms2s import s2hms
+
 in_path = Path(sys.argv[1])
-out_path = Path(f'ic_{sys.argv[1]}')
+out_path = in_path.parents[0] / Path(f'ic_{in_path.name}')
 
 hum_json = json.loads(in_path.read_text())
 ic_json = []
@@ -12,14 +14,20 @@ for track in hum_json['media'][0]['tracks']:
     for e in track['trackEvents']:
         options = {}
         if e['type'] == 'mutePlugin':
+            start = e['popcornOptions']['start']
+            end = e['popcornOptions']['end']
+            options['label'] = f'{s2hms(start)} - {s2hms(end)}'
             options['type'] = 'mute'
-            options['start'] = str(e['popcornOptions']['start'])
-            options['end'] = str(e['popcornOptions']['end'])
+            options['start'] = str(start)
+            options['end'] = str(end)
             options['details'] = {}
         elif e['type'] == 'skip':
+            start = e['popcornOptions']['start']
+            end = e['popcornOptions']['end']
+            options['label'] = f'{s2hms(start)} - {s2hms(end)}'
             options['type'] = 'skip'
-            options['start'] = str(e['popcornOptions']['start'])
-            options['end'] = str(e['popcornOptions']['end'])
+            options['start'] = str(start)
+            options['end'] = str(end)
             options['details'] = {}
         else:
             raise NotImplementedError(f'Event "{e["type"]}" not implemented.')
