8468d24a6a503bb76c782ff0dd30b9bf0085e1af
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2018-11-29 17:15:00 -0700

    for converting between HH:MM:SS and secs

---
+++ scripts/hms2s.py
@@ -0,0 +1,40 @@
+"""Convert between HH:MM:SS and seconds."""
+import datetime as dt
+import re
+import sys
+
+
+def hms2s(time_str):
+    """Convert HH:MM:SS times to seconds."""
+    output = [time_str]
+    for t in re.findall('([0-9:.]+)', time_str, flags=re.S):
+        times = [int(i) for i in t.split(':')]
+        size = len(times)
+        if size > 3:
+            raise NotImplementedError('Expected input: HH:MM:SS or MM:SS.')
+        elif size == 3:
+            h, m, s = times
+        elif size == 2:
+            h = 0
+            m, s = times
+        elif size == 1:
+            h, m = 0, 0
+            s = times[0]
+        else:
+            continue
+        tot = int(dt.timedelta(hours=h, minutes=m, seconds=s).total_seconds())
+        output.append(tot)
+    return output
+
+
+def s2hms(secs, rounding=True):
+    """Convert seconds to HH:MM:SS."""
+    if rounding:
+        secs = round(secs)
+    return str(dt.timedelta(seconds=secs))
+
+
+if __name__ == '__main__':
+    in_lines = sys.stdin.readlines()
+    for line in in_lines:
+        print(*hms2s(line.rstrip()), sep='\t')
